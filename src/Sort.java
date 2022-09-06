5dfd49ba8db2099ec6a2b56c9d60409257a64650
natedog13056 <jinathan188@gmail.com>
2022-09-05 21:09:51 -0400

    using Arrays.toString

--- src/Sort.java
+++ src/Sort.java
@@ -1,18 +1,20 @@
+import java.util.Arrays;
+
 public class Sort {
     public static void main(String[] args) {
         System.out.println("Sorting Arrays Project : ");
 
         int[] data = new int[]{4, 2, 3, 6, 9, 3, 5};
 
-
-        Sort printing = new Sort();
-
         System.out.println("BubbleSorting:");
-        printing.print(Sort.bubblesort(data));
+        Sort.bubblesort(data);
+        System.out.println(Arrays.toString(data));
         System.out.println("\nInsertionSorting");
-        printing.print(Sort.insertionsort(data));
+        Sort.insertionsort(data);
+        System.out.println(Arrays.toString(data));
         System.out.println("\nSelectionSorting");
-        printing.print(Sort.selectionsort(data));
+        Sort.selectionsort(data);
+        System.out.println(Arrays.toString(data));
     }
 
     public static int[] bubblesort(int[] data) {
@@ -33,30 +35,29 @@ public class Sort {
     }
 
     public static int[] insertionsort(int[] data) {
-        for (int i = 1; i < data.length; i++  ) {
+        for (int i = 1; i < data.length; i++) {
             int current = data[i];
-            int y = i-1;
-            while( y >=0 && current < data[y]){
-                data[y+1] = data[y];
+            int y = i - 1;
+            while (y >= 0 && current < data[y]) {
+                data[y + 1] = data[y];
 
                 y--;
             }
-            data[y+1] = current;
+            data[y + 1] = current;
         }
 
         return data;
     }
 
     public static int[] selectionsort(int[] data) {
-        for(int z = 0; z < data.length; z++){
+        for (int z = 0; z < data.length; z++) {
             int minimum = data[z];
             int minimumID = z;
-            for(int j = z+1; j < data.length; j++){
-                if(data[j] < minimum){
+            for (int j = z + 1; j < data.length; j++) {
+                if (data[j] < minimum) {
                     minimum = data[j];
                     minimumID = j;
                 }
-
             }
 
             int temporary = data[z];
@@ -64,14 +65,5 @@ public class Sort {
             data[minimumID] = temporary;
         }
         return data;
-    }
-
-
-
-
-    public void print(int[] data) {
-        for (int value : data) {
-            System.out.print(value + ",");
-        }
     }
 }
